e80a6bf2b94ef22e489e9726686527a992e3103a
superbluesea <349200964@qq.com>
2017-09-11 00:01:44 +0800

    游标实现链表

---
+++ Cursor.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"Cursor.h"
+
+#define SpaceSize 10
+
+struct Node
+{
+	ElementType Element;
+	Position Next;
+};
+
+struct Node CursorSpace[SpaceSize];
+
+int main()
+{
+
+	return 0;
+}
---
+++ Cursor.h
@@ -0,0 +1,23 @@
+#ifndef _Cursor_H
+
+typedef int PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+typedef int ElementType;
+
+void InitializeCursorSpace(void);
+
+List MakeEmpty(List L);
+int IsEmpty(const List L);
+int IsLast(const Position P, const List L);
+Position Find(ElementType X, const List L);
+void Delete(ElementType X, List L);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
+Position Header(const List L);
+Position First(const List L);
+Position Advance(const Position P);
+ElementType Retrieve(const Position P);
+
+#endif // !_Cursor_H
