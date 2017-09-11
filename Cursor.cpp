a7f384186e2939425262be484308c629c2f146de
superbluesea <349200964@qq.com>
2017-09-11 15:52:23 +0800

    add

--- Cursor.cpp
+++ Cursor.cpp
@@ -11,6 +11,53 @@ struct Node
 
 struct Node CursorSpace[SpaceSize];
 
+static Position CursorAlloc(void)
+{
+	Position P;
+	P = CursorSpace[0].Next;
+	CursorSpace[0].Next = CursorSpace[P].Next;
+	return P;
+}
+
+static void CursorFree(Position P)
+{
+	CursorSpace[P].Next = CursorSpace[0].Next;
+	CursorSpace[0].Next = P;
+}
+
+int IsEmpty(List L)
+{
+	return CursorSpace[0].Next == 0;
+}
+
+int IsLast(Position P, List L)
+{
+	return CursorSpace[P].Next == 0;
+}
+
+void Delete(ElementType X, List L)
+{
+	Position P, TmpCell;
+	P = FindPrevious(X, L);
+	if (!IsLast(P, L))
+	{
+		TmpCell = CursorSpace[P].Next;
+		CursorSpace[P].Next = CursorSpace[TmpCell].Next;
+		CursorFree(TmpCell);
+	}
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+	Position TmpCell;
+	TmpCell = CursorAlloc();
+	if (TmpCell == 0)
+		printf("Out of space");
+	CursorSpace[TmpCell].Element = X;
+	CursorSpace[TmpCell].Next = CursorSpace[P].Next;
+	CursorSpace[P].Next = TmpCell;
+}
+
 int main()
 {
 
--- Cursor.h
+++ Cursor.h
@@ -1,4 +1,5 @@
-#ifndef _Cursor_H
+#ifndef _Cursor_H_
+#define _Cursor_H_
 
 typedef int PtrToNode;
 typedef PtrToNode List;
@@ -13,6 +14,7 @@ int IsEmpty(const List L);
 int IsLast(const Position P, const List L);
 Position Find(ElementType X, const List L);
 void Delete(ElementType X, List L);
+Position FindPrevious(ElementType X, const List L);
 void Insert(ElementType X, List L, Position P);
 void DeleteList(List L);
 Position Header(const List L);
@@ -20,4 +22,4 @@ Position First(const List L);
 Position Advance(const Position P);
 ElementType Retrieve(const Position P);
 
-#endif // !_Cursor_H
+#endif
