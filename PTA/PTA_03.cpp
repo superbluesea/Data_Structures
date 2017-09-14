a981d1d54ab30bdeb481bfc94815b58c3973274d
superbluesea <349200964@qq.com>
2017-09-14 11:06:59 +0800

    PTA_03
    1、要修改指针的值必须传入指针的地址(二重指针)；
    2、注意输入为0的情况(0个多项式)。

--- PTA/PTA_03.cpp
+++ PTA/PTA_03.cpp
@@ -1,62 +1,157 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-#define MaxDegree 2000
+typedef struct Node *PtrToNode;
 
-typedef struct{
-	int CoeffArray[MaxDegree + 1];
-	int HighPower;
-}*Polynomial;
+struct Node{
+	int Coefficient;
+	int Exponent;
+	PtrToNode Next;
+};
 
-void ZeroPolynomial(Polynomial Poly)
+typedef PtrToNode Polynomial;
+
+void Read(Polynomial L)
 {
-	for (int i = 0; i <= MaxDegree; ++i)
-		Poly->CoeffArray[i] = 0;
-	Poly->HighPower = 0;
-}
-
-void AddPolynomial(const Polynomial Poly1, const Polynomial Poly2, Polynomial PolySum)
-{
-	PolySum->HighPower = Poly1->HighPower > Poly2->HighPower ? Poly1->HighPower : Poly2->HighPower;
-	for (int i = 0; i <= PolySum->HighPower; ++i)
-		PolySum->CoeffArray[i] = Poly1->CoeffArray[i] + Poly2->CoeffArray[i];
-}
-
-void MultPolynomial(const Polynomial Poly1, const Polynomial Poly2, Polynomial PolyProd)
-{
-	PolyProd->HighPower = Poly1->HighPower*Poly2->HighPower;
-	for (int i = 0; i <= Poly1->HighPower; ++i)
+	PtrToNode tmp = L;
+	int n;
+	scanf("%d", &n);
+	while (n--)
 	{
-		for (int j = 0; j <= Poly2->HighPower; ++j)
+		PtrToNode Ptr = (PtrToNode)malloc(sizeof(struct Node));
+		int c, e;
+		scanf("%d %d", &c, &e);
+		Ptr->Exponent = e;
+		Ptr->Coefficient = c;
+		Ptr->Next = NULL;
+		tmp->Next = Ptr;
+		tmp = Ptr;
+	}
+}
+
+void Attach(PtrToNode *Ptr,int e,int c)
+{
+	PtrToNode p = (PtrToNode)malloc(sizeof(struct Node));
+	p->Next = (*Ptr)->Next;
+	p->Exponent = e;
+	p->Coefficient = c;
+	(*Ptr)->Next = p;
+	*Ptr = p;
+}
+
+Polynomial Add(Polynomial L1, Polynomial L2)
+{
+	Polynomial PA = (Polynomial)malloc(sizeof(struct Node));
+	PA->Next = NULL;
+	Polynomial t1 = L1->Next, t2 = L2->Next, t3 = PA;
+	while (t1&&t2)
+	{
+		if (t1->Exponent == t2->Exponent)
 		{
-			PolyProd->CoeffArray[i + j] += Poly1->CoeffArray[i] * Poly2->CoeffArray[j];
+			if (t1->Coefficient+t2->Coefficient!=0)
+				Attach(&t3, t1->Exponent, t1->Coefficient + t2->Coefficient);
+			t1 = t1->Next;
+			t2 = t2->Next;
+		}
+		else if (t1->Exponent > t2->Exponent)
+		{
+			Attach(&t3, t1->Exponent, t1->Coefficient);
+			t1 = t1->Next;
+		}
+		else
+		{
+			Attach(&t3, t2->Exponent, t2->Coefficient);
+			t2 = t2->Next;
 		}
 	}
+	while (t1)
+	{
+		Attach(&t3, t1->Exponent, t1->Coefficient);
+		t1 = t1->Next;
+	}
+	while (t2)
+	{
+		Attach(&t3, t2->Exponent, t2->Coefficient);
+		t2 = t2->Next;
+	}
+	return PA;
+}
+
+Polynomial Mult(Polynomial L1, Polynomial L2)
+{
+	Polynomial PM = (Polynomial)malloc(sizeof(struct Node));
+	PM->Next = NULL;
+	PtrToNode t1 = L1->Next, t2 = L2->Next, t3 = PM;
+	if (!t1 || !t2)return PM;
+	while (t2)
+	{
+		Attach(&t3, t1->Exponent + t2->Exponent, t1->Coefficient * t2->Coefficient);
+		t2 = t2->Next;
+	}
+	t1 = t1->Next;
+	while (t1)
+	{
+		t2 = L2->Next;
+		t3 = PM;
+		while (t2)
+		{
+			int e = t1->Exponent + t2->Exponent, c = t1->Coefficient * t2->Coefficient;
+			while (t3->Next&&t3->Next->Exponent > e)
+				t3 = t3->Next;
+			if (t3->Next&&t3->Next->Exponent == e)
+			{
+				t3->Next->Coefficient += c;
+				if (t3->Next->Coefficient == 0)
+				{
+					PtrToNode tmp = t3->Next;
+					t3->Next = tmp->Next;
+					free(tmp);
+				}
+			}
+			else
+			{
+				Attach(&t3, e, c);
+			}
+			t2 = t2->Next;
+		}
+		t1 = t1->Next;
+	}
+	return PM;
+}
+
+void Print(Polynomial L)
+{
+	PtrToNode tmp = L->Next;
+	int flag = 1;
+	if (tmp == NULL)
+	{
+		printf("0 0\n");
+		return;
+	}
+	while (tmp)
+	{
+		if (flag)
+			flag = 0;
+		else
+			printf(" ");
+		printf("%d %d", tmp->Coefficient, tmp->Exponent);
+		tmp = tmp->Next;
+	}
+	printf("\n");
 }
 
 int main()
 {
-	Polynomial p1, p2;
-	ZeroPolynomial(p1);
-	ZeroPolynomial(p2);
-	int n = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i)
-	{
-		int tmp1, tmp2;
-		scanf("%d %d", &tmp1, &tmp2);
-		p1->CoeffArray[tmp2] = tmp1;
-		if (tmp2>p1->HighPower)
-			p1->HighPower = tmp2;
-	}
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i)
-	{
-		int tmp1, tmp2;
-		scanf("%d %d", &tmp1, &tmp2);
-		p2->CoeffArray[tmp2] = tmp1;
-		if (tmp2>p2->HighPower)
-			p2->HighPower = tmp2;
-	}
-
+	Polynomial P1, P2, PA, PM;
+	P1 = (Polynomial)malloc(sizeof(struct Node));
+	P1->Next = NULL;
+	P2 = (Polynomial)malloc(sizeof(struct Node));
+	P2->Next = NULL;
+	Read(P1);
+	Read(P2);
+	PA = Add(P1, P2);
+	PM = Mult(P1, P2);
+	Print(PM);
+	Print(PA);
 	return 0;
 }
